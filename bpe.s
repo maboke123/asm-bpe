79159be9b0692aaa8108a7a5e9deb420cc66eca9
Mathias <mathias.bouhon@gmail.com>
2026-02-07 21:16:18 +0100

    startup and showing user input + .txt

--- bpe.s
+++ bpe.s
@@ -1,19 +1,18 @@
 .data
-msg2:
-    .asciz "haha\n"
-msg2_end:
-msg2_length = msg2_end - msg2
+    user_input:
+        .asciz "aabcdefgh"
+    user_input_end:
+    user_input_length = user_input_end - user_input
 
-msg:
-    .asciz "hello world\n"
-msg_end:
-msg_length = msg_end - msg
+    newline:
+        .asciz "\n"
+    newline_end:
+    newline_length = newline_end - newline
 
-msg3:
-    .asciz "nice\n"
-msg_end3:
-msg3_length = msg_end3 - msg3
-
+    placeholder_msg:
+        .asciz "Original message: "
+    placeholder_msg_end:
+    placeholder_msg_length = placeholder_msg_end - placeholder_msg
 
 .text
 .global _start
@@ -21,25 +20,32 @@ msg3_length = msg_end3 - msg3
 _start:
     li a7, 64
     li a0, 1
-    la a1, msg2
-    li a2, msg2_length
+    la a1, placeholder_msg
+    li a2, placeholder_msg_length
     ecall
 
     li a7, 64
     li a0, 1
-    la a1, msg
-    li a2, msg_length
+    la a1, user_input
+    li a2, user_input_length
     ecall
-    
-    li a7, 64
-    li a0, 1
-    la a1, msg3
-    li a2, msg3_length
-    ecall
+
+    jal ra, print_newline
 
     li a7, 93
     li a0, 0
     ecall
+
+print_newline:
+
+    li a7, 64
+    li a0, 1
+    la a1, newline
+    li a2, newline_length
+    ecall
+
+    ret
+
 
 # riscv64-linux-gnu-as bpe.s -o bpe.o
 # riscv64-linux-gnu-ld -T linker.ld bpe.o -o bpe
